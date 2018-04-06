90fff37a10bf72b8bc8faa698d213fa6ed99e2aa
annterina <anna.julia.lesniak@gmail.com>
2018-04-06 17:01:55 +0200

    Change test to transactional

--- src/test/java/foodambulance/dao/CustomerDAOImplTest.java
+++ src/test/java/foodambulance/dao/CustomerDAOImplTest.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.HashSet;
@@ -17,6 +18,7 @@ import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:dispatcher-servlet.xml")
+@Transactional
 public class CustomerDAOImplTest {
 
     @Autowired
